be6b5fdc4def7fd92dfa99324880d898420eb42e
Selorm <ranselorm@outlook.com>
2024-01-23 09:45:44 +0000

    Fix layout on mobile

--- components/Graduation.jsx
+++ components/Graduation.jsx
@@ -11,19 +11,21 @@ const Graduation = () => {
   return (
     <section className="my-[100px]">
       <div className="flex flex-col-reverse md:flex-row justify-center items-center gap-10 ">
-        <article className="md:w-5/12 flex flex-col gap-4 items-start">
+        <article className="md:w-5/12 flex flex-col gap-4 md:items-start items-center">
           <h3 className="font-semibold text-[20px] lg:text-[25px] flex items-center gap-3">
             Graduation Time
             <GiGraduateCap className="text-[#FAC97A]" />
           </h3>
-          <p className="text-left">
+          <p className="md:text-left text-center">
             I am committed to delivering a personalized and memorable
             experience, ensuring that you look and feel your best as you
-            celebrate this significant achievement. Don't miss the opportunity
-            to secure your spot and make your graduation day even more
-            extraordinary with a professionally crafted makeup look. Book now
-            and let's make your graduation day a beautiful and
-            unforgettable experience!
+            celebrate this significant achievement.
+          </p>
+          <p className="md:text-left text-center">
+            Don't miss the opportunity to secure your spot and make your
+            graduation day even more extraordinary with a professionally crafted
+            makeup look. Book now and let's make your graduation day a beautiful
+            and unforgettable experience!
           </p>
           {/* <input
               type="text"
@@ -31,13 +33,13 @@ const Graduation = () => {
               placeholder="Enter your email address"
             /> */}
           <button
-            className="bg-btn-gradient py-2 px-3 rounded-full text-black text-sm font-semibold w-4/12"
+            className="bg-btn-gradient py-2 px-3 rounded-full text-black text-sm font-semibold md:w-4/12 w-full"
             onClick={() => setIsModalOpen(true)}
           >
             Register
           </button>
         </article>
-        <div className="w-full md:w-4/12">
+        <div className="w-full md:w-4/12 flex justify-center">
           <div className="relative w-[350px] h-[350px] rounded-lg overflow-hidden">
             <Image src="/grad.jpg" alt="" fill className="object-cover" />
           </div>
